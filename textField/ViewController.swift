e7ff32b1db27605d3ca189876decdc97ecfe56ae
Victoria Mahoney <63237@psdschools.org>
2022-02-01 14:18:41 -0700

    work in progress

--- textField/ViewController.swift
+++ textField/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var labelOutlet1: UILabel!
+    
+
+    @IBOutlet weak var textField: UITextField!
+    
+  //viewDidLoad function
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
+    //button function
+    @IBAction func buttonClick(_ sender: Any) {
+    }
+    
+    
+    
+    
 
 }
 
